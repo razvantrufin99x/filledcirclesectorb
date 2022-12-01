8ed690ffb3552964217d5dce7ea4a4aa126bc94e
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:22:56 +0200

    Add project files.

---
+++ filledcirclesector/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 6/17/2020
+ * Time: 12:28 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace filledcirclesector
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(103, 103);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1Paint);
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseClick);
+			this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseDoubleClick);
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseDown);
+			this.pictureBox1.MouseEnter += new System.EventHandler(this.PictureBox1MouseEnter);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseMove);
+			this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseUp);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 200;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(28, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(46, 25);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "100";
+			this.label1.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(104, 104);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Cursor = System.Windows.Forms.Cursors.Cross;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "MainForm";
+			this.Text = "filledcirclesector";
+			this.TransparencyKey = System.Drawing.SystemColors.Control;
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.Shown += new System.EventHandler(this.MainFormShown);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ filledcirclesector/MainForm.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 6/17/2020
+ * Time: 12:28 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace filledcirclesector
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		public Graphics g;
+		public Font font0;
+		public Pen pen0 = new Pen(Color.Black);
+		public Pen pen1 = new Pen(Color.Black,7);
+		public Pen pen2 = new Pen(Color.Red,30);
+		public Brush brush0 =  new SolidBrush(Color.Black);
+		public float cx,cy,px,py;
+		public float step = 5.0f;
+		public const double g2r = 180 / Math.PI;
+		public int ismd = 0;
+		public int su = 0;
+		public int eu = 100;
+		
+		public void drawcircle()
+		{
+			cx = (float)Math.Cos(0/g2r) * 25*2 + 100;
+			cy = (float)Math.Sin(0/g2r) * 25*2 + 100;
+			px = cx;
+			py = cy;
+			
+			for(float i = 0 ; i < 360+step ; i+=step)
+			{
+				cx = (float)Math.Cos(i/g2r) * 25*2 + 100;
+				cy = (float)Math.Sin(i/g2r) * 25*2 + 100;
+				try{
+					g.DrawLine(pen0,cx,cy,px,py);
+				}
+				catch{}
+				px = cx;
+				py = cy;
+			}
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			font0 = this.Font;
+			g = pictureBox1.CreateGraphics();
+			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+			
+			
+		}
+		void MainFormShown(object sender, EventArgs e)
+		{
+			
+		}
+		void PictureBox1MouseClick(object sender, MouseEventArgs e)
+		{
+			
+		}
+		void PictureBox1Paint(object sender, PaintEventArgs e)
+		{
+			
+		}
+		void PictureBox1MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Close();
+		}
+		void PictureBox1MouseEnter(object sender, EventArgs e)
+		{
+			
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			//drawcircle();
+			g.Clear(BackColor);
+			g.DrawEllipse(pen0,0,0,100,100);
+			g.DrawEllipse(pen0,10,10,80,80);
+			g.DrawArc(pen1,10,10,80,80,su++,eu++);
+			//g.DrawArc(pen2,10,10,80,80,su++,eu++);
+			g.DrawArc(pen2,10,10,80,80,su,2);
+			g.DrawArc(pen2,10,10,80,80,eu,2);
+			g.DrawString(su.ToString(),font0,brush0,40,33);
+			g.DrawString(eu.ToString(),font0,brush0,40,47);
+			label1.Text = (eu-su).ToString();
+			
+			if(su<360){
+				su++;
+			}
+			else{su=0;}
+			if(eu<360){
+				eu+=2;
+			}
+			else{eu=su+1;}
+			//pictureBox1.Invalidate();
+		}
+		void PictureBox1MouseUp(object sender, MouseEventArgs e)
+		{
+			ismd = 0;
+		}
+		void PictureBox1MouseDown(object sender, MouseEventArgs e)
+		{
+			ismd = 1;
+		}
+		void PictureBox1MouseMove(object sender, MouseEventArgs e)
+		{
+			if(ismd ==1)
+			{
+				Left += e.X;
+				Top  += e.Y;
+			}
+		}
+	}
+}
